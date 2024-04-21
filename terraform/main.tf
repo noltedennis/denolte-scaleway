5297497a5b60361a7a429db248981b1adc04488e
Dennis Nolte <dennis_nolte@gmx.de>
2024-04-21 22:52:04 +0200

    run terraform fmt

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,54 +1,54 @@
-resource scaleway_account_project "this" {
+resource "scaleway_account_project" "this" {
   name = "denolte"
 }
 
 resource "scaleway_vpc" "this" {
-  name = "denolte"
+  name       = "denolte"
   project_id = scaleway_account_project.this.id
 }
 
 resource "scaleway_vpc_private_network" "this" {
-  name = "kapsule"
+  name       = "kapsule"
   project_id = scaleway_account_project.this.id
-  vpc_id = scaleway_vpc.this.id
+  vpc_id     = scaleway_vpc.this.id
   ipv4_subnet {
     subnet = "10.0.0.0/8"
   }
 }
 
 resource "scaleway_vpc_public_gateway" "this" {
-  name = "denolte"
-  type = "VPC-GW-S"
+  name       = "denolte"
+  type       = "VPC-GW-S"
   project_id = scaleway_account_project.this.id
 }
 
-resource scaleway_vpc_public_gateway_ip "this" {
+resource "scaleway_vpc_public_gateway_ip" "this" {
   project_id = scaleway_account_project.this.id
 }
 
 resource "scaleway_k8s_cluster" "this" {
-  name    = "denolte"
-  type    = "kapsule"
-  version = "1.29"
-  cni     = "cilium"
-  private_network_id = scaleway_vpc_private_network.this.id
+  name                        = "denolte"
+  type                        = "kapsule"
+  version                     = "1.29"
+  cni                         = "cilium"
+  private_network_id          = scaleway_vpc_private_network.this.id
   delete_additional_resources = true
   auto_upgrade {
-    enable = true
+    enable                        = true
     maintenance_window_start_hour = 3
-    maintenance_window_day = "any"
+    maintenance_window_day        = "any"
   }
   project_id = scaleway_account_project.this.id
 }
 
 resource "scaleway_k8s_pool" "dev1m" {
-  cluster_id = scaleway_k8s_cluster.this.id
-  name       = "dev1m"
-  node_type  = "DEV1-M"
-  autoscaling = true
-  autohealing = true
-  size = 1
-  min_size    = 1
-  max_size    = 1
+  cluster_id         = scaleway_k8s_cluster.this.id
+  name               = "dev1m"
+  node_type          = "DEV1-M"
+  autoscaling        = true
+  autohealing        = true
+  size               = 1
+  min_size           = 1
+  max_size           = 1
   public_ip_disabled = true
 }
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,16 +1,16 @@
 terraform {
   required_providers {
     scaleway = {
-      source = "scaleway/scaleway"
+      source  = "scaleway/scaleway"
       version = "~> 2.39.0"
     }
   }
   required_version = ">= 1.8"
 
   backend "s3" {
-    bucket                      = "denolte-terraform"
-    key                         = "kapsule.tfstate"
-    region                      = "fr-par"
+    bucket = "denolte-terraform"
+    key    = "kapsule.tfstate"
+    region = "fr-par"
     endpoints = {
       s3 = "https://s3.fr-par.scw.cloud"
     }
@@ -21,7 +21,7 @@ terraform {
 }
 
 provider "scaleway" {
-  zone   = "fr-par-1"
-  region = "fr-par"
-  organization_id    = "3bfd4112-c8a8-41df-af67-f7fb7271648f"
+  zone            = "fr-par-1"
+  region          = "fr-par"
+  organization_id = "3bfd4112-c8a8-41df-af67-f7fb7271648f"
 }
